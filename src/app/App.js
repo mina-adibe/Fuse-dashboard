db4be10b5c992fd39190758bbd7cc523ba832cb4
mina bessada <mina.adibe@gmail.com>
2022-01-17 00:50:19 +0200

    add axios and delete fake db

--- src/app/App.js
+++ src/app/App.js
@@ -1,34 +1,35 @@
-import '@fake-db';
-import FuseAuthorization from '@fuse/core/FuseAuthorization';
-import FuseLayout from '@fuse/core/FuseLayout';
-import FuseTheme from '@fuse/core/FuseTheme';
-import history from '@history';
-import { Router } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
-import { useSelector } from 'react-redux';
-import rtlPlugin from 'stylis-plugin-rtl';
-import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
-import { selectCurrLangDir } from 'app/store/i18nSlice';
-import withAppProviders from './withAppProviders';
-import { Auth } from './auth';
+import FuseAuthorization from "@fuse/core/FuseAuthorization";
+import FuseLayout from "@fuse/core/FuseLayout";
+import FuseTheme from "@fuse/core/FuseTheme";
+import history from "@history";
+import { Router } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import { useSelector } from "react-redux";
+import rtlPlugin from "stylis-plugin-rtl";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { selectCurrLangDir } from "app/store/i18nSlice";
+import axios from "axios";
+import withAppProviders from "./withAppProviders";
+import { Auth } from "./auth";
 
 // import axios from 'axios';
 /**
  * Axios HTTP Request defaults
  */
-// axios.defaults.baseURL = "";
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-// axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.headers.common["x-api-key"] = "F0CF9981-49BF-475F-B338-691D05F98520";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+// axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 const emotionCacheOptions = {
   rtl: {
-    key: 'muirtl',
+    key: "muirtl",
     stylisPlugins: [rtlPlugin],
     prepend: true,
   },
   ltr: {
-    key: 'muiltr',
+    key: "muiltr",
     stylisPlugins: [],
     prepend: true,
   },
@@ -46,11 +47,11 @@ const App = () => {
               <SnackbarProvider
                 maxSnack={5}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
+                  vertical: "bottom",
+                  horizontal: "right",
                 }}
                 classes={{
-                  containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
+                  containerRoot: "bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99",
                 }}
               >
                 <FuseLayout />
