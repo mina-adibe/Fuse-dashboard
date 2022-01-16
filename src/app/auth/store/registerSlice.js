dbf9bc5637e23886984c36af896075fd97fb7ad3
mina bessada <mina.adibe@gmail.com>
2022-01-17 01:43:49 +0200

    removr createUserSettingsFirebase

--- src/app/auth/store/registerSlice.js
+++ src/app/auth/store/registerSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { showMessage } from "app/store/fuse/messageSlice";
 import jwtService from "app/services/jwtService";
-import { createUserSettingsFirebase, setUserData } from "./userSlice";
+import { setUserData } from "./userSlice";
 
 export const submitRegister =
   ({ displayName, password, email }) =>
